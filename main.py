3d89d6e15e195d02581e56cb7004e1af7e864ad1
Alican BALIK <alicanbalik@outlook.com>
2022-06-17 11:15:14 +0200

    Enabled hotkey.#

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import datetime
+from time import sleep
 
 import cv2 as cv
+import win32api
+import win32con
+import win32gui
 
 from object_detection import ObjectDetection
 from windowcapture import WindowCapture
@@ -10,6 +14,9 @@ bot = ObjectDetection()
 
 window.start()
 bot.start()
+
+window_name = "Rise Online Client"
+hwnd = win32gui.FindWindow(None, window_name)
 
 
 class State:
@@ -21,15 +28,27 @@ class State:
     RESET = 5
 
 
+fishing_started = False
+
 while True:
     if window.screenshot is None:
         continue
 
+    # http://kbdedit.com/manual/low_level_vk_list.html
+
     if bot.current_state == State.IN_GAME:
         bot.update(window.screenshot)
+
+        if not fishing_started:
+            win32api.SendMessage(hwnd, win32con.WM_KEYDOWN, 0x46 , 0)
+            sleep(0.5)
+            win32api.SendMessage(hwnd, win32con.WM_KEYUP, 0x46 , 0)
+            fishing_started = True
+
         disconnected = bot.is_disconnected()
 
         if disconnected:
+            fishing_started = False
             print('Disconnected at {}.'.format(datetime.datetime.now()))
 
             bot.update_state(State.LOGIN)
